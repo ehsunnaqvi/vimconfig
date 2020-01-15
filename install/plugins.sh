3762199b3c5ffecf9ff0251ec81ae919b231dd28
Ahsan Naqvi <ahsan.naqvi15@gmail.com>
2020-01-15 14:00:14 -0500

    Use dadbod for interaction with db

--- install/plugins.sh
+++ install/plugins.sh
@@ -5,10 +5,9 @@ git clone https://github.com/tpope/vim-rails.git ~/.vim/bundle/vim-rails
 git clone https://tpope.io/vim/sleuth.git ~/.vim/bundle/sleuth
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://tpope.io/vim/unimpaired.git ~/.vim/bundle/unimpaired
-
+git clone https://tpope.io/vim/dadbod.git ~/.vim/bundle/dadbod
 # Python plugins
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
 git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
-
 # Git plugin
 git clone https://tpope.io/vim/fugitive.git ~/.vim/bundle/fugitive
