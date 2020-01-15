f53e0610be353ce3d6e6142d893dfc54ae9ad4b1
Ahsan Naqvi <ahsan.naqvi15@gmail.com>
2020-01-15 12:10:29 -0500

    Add unimpaired for better paste

--- install/plugins.sh
+++ install/plugins.sh
@@ -4,7 +4,11 @@ git clone git://github.com/tpope/vim-bundler.git ~/.vim/bundle/vim-bundler
 git clone https://github.com/tpope/vim-rails.git ~/.vim/bundle/vim-rails
 git clone https://tpope.io/vim/sleuth.git ~/.vim/bundle/sleuth
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://tpope.io/vim/unimpaired.git ~/.vim/bundle/unimpaired
 
 # Python plugins
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
 git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
+
+# Git plugin
+git clone https://tpope.io/vim/fugitive.git ~/.vim/bundle/fugitive
